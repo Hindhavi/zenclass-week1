6abdb1f8fcc067d215ad3837c771b897e4bb25aa
Hindhavi Alluru <67951932+Hindhavi@users.noreply.github.com>
2020-07-25 04:09:03 +0530

    Add files via upload

---
+++ 24-07-2020/typeofdt.js
@@ -0,0 +1,31 @@
+var a="Hindhavi";
+console.log(typeof a);    //string
+
+var b=Date(1);
+console.log(typeof b);    //string
+
+var c="hello world 20";
+console.log(typeof c);    //string
+
+var d= 99;
+console.log(typeof d); //number
+
+var e= NaN;
+console.log(typeof e); //number
+
+var f= true;
+console.log(typeof f); //boolean
+
+var g= {
+    name:"hindhavi"
+};
+console.log(typeof g); //object
+
+var h=[1,2,3,5];
+console.log(typeof h); //Object
+
+var i=null;
+console.log(typeof i); //Object
+
+var j;
+console.log(typeof j); //undefined
